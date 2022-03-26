7e557005f76a911b71b6a31940405e9686edff0f
Albe MARINI-AUDOUARD <amarini-@e2r11p19.clusters.42paris.fr>
2022-03-26 17:36:06 +0100

    🎨 -Changed variable name for amount of time lapsed in wait function, it's name was "test" which isn't great

--- srcs/thread_routine/wait.c
+++ srcs/thread_routine/wait.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 23:31:35 by amarini-          #+#    #+#             */
-/*   Updated: 2022/03/26 17:33:23 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/03/26 17:35:56 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 int	wait_time(long given, t_info *info)
 {
 	long	time;
-	long	test;
+	long	lapsed;
 
-	test = get_current_time();
+	lapsed = get_current_time();
 	time = 0;
 	while (time < given)
 	{
 		if (philo_check_death(info) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
-		if ((get_current_time() - test) >= given)
+		if ((get_current_time() - lapsed) >= given)
 			break ;
 		usleep(1000);
 		++time;
