d823afbd2bf347e74b638b4231d7db109f4ad476
Albe MARINI-AUDOUARD <amarini-@e2r11p19.clusters.42paris.fr>
2022-03-26 12:43:19 +0100

    🚧 -Still figuring out bugs, need to make some performance changes, valgrind is making everything fail (it should not happen :C)

--- srcs/thread_routine/god_routine.c
+++ srcs/thread_routine/god_routine.c
@@ -6,7 +6,7 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 01:48:16 by amarini-          #+#    #+#             */
-/*   Updated: 2022/03/26 12:30:36 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/03/26 12:41:00 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,10 @@ int	check_all_alive(t_info *all_info, t_philo *philo)
 	while (i < all_info->nbrp)
 	{
 		pthread_mutex_lock(&(philo[i].meal));
-		elapsed = get_current_time() - philo[i].last_meal;
+		if (philo[i].last_meal == 0)
+			elapsed = 0;
+		else
+			elapsed = get_current_time() - philo[i].last_meal;
 		pthread_mutex_unlock(&(philo[i].meal));
 		if (elapsed >= all_info->time_die)
 		{
